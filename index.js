6ed28eb7627ef5e7da6a45c5a1b87fca163a853c
Fernando Rodrigues da Silva <fernanndo.sillva@gmail.com>
2023-06-12 17:56:45 -0300

    Atualizado
    Atualizado

--- index.js
+++ index.js
@@ -2,84 +2,95 @@ const Devs = [];
 
 const button = document.getElementById("addTech");
 
-button.addEventListener("click", criar);
+button.addEventListener("click", function (ev) {
+  ev.preventDefault();
 
-function criar() {
-  const form = document.getElementById("input");
+  const section = document.getElementById("section");
 
-  const nome = document.createElement("label");
+  const form = document.createElement("form");
 
-  nome.innerText = "Tecnologia : ";
+  const h3 = document.createElement("h3");
+  h3.innerText = "Adicionar Experiencia";
+
+  const tecnologia = document.createElement("label");
+  tecnologia.innerText = "Tecnologia: ";
 
   const tech = document.createElement("input");
   tech.name = "Tecnologia";
-  tech.type = "input";
-  const hr = document.createElement("hr");
+  tech.type = "text";
 
   const time = document.createElement("label");
   time.innerText = " | Tempo de Experiencia:";
 
-  const radioInput = document.createElement("input");
-  radioInput.name = "checado";
-  radioInput.type = "radio";
+  const input1 = document.createElement("input");
+  input1.name = "check";
+  input1.id = "check";
+  input1.type = "radio";
+  input1.value = "0-2 anos";
 
   const radio1 = document.createElement("label");
-  radio1.name = "main";
+  radio1.name = "check";
+  radio1.id = "check";
   radio1.innerText = "0 - 2 anos";
 
-  const radioInput1 = document.createElement("input");
-  radioInput1.name = "checado";
-  radioInput1.type = "radio";
+  const input2 = document.createElement("input");
+  input2.name = "check";
+  input2.id = "check";
+  input2.type = "radio";
+  input2.value = "3-5 anos";
 
   const radio2 = document.createElement("label");
-  radio2.name = "main";
+  radio2.name = "check";
+  radio2.id = "check";
   radio2.innerText = "3 - 5 anos";
 
-  const radioInput2 = document.createElement("input");
-  radioInput2.name = "checado";
-  radioInput2.type = "radio";
+  const input3 = document.createElement("input");
+  input3.name = "check";
+  input3.id = "check";
+  input3.type = "radio";
+  input3.value = "10 anos";
 
   const radio3 = document.createElement("label");
-  radio3.name = "main";
+  radio3.name = "check";
+  radio3.id = "check";
   radio3.innerText = "10 anos - ";
-
-  const radioInput3 = document.createElement("input");
-  radioInput3.name = "checado";
-  radioInput3.type = "radio";
 
   const buttonExcluir = document.createElement("button");
   buttonExcluir.innerText = "Excluir";
+  buttonExcluir.id = "excluir";
+  buttonExcluir.name = "excluir";
 
-  nome.appendChild(tech);
-  nome.appendChild(time);
-  nome.appendChild(radioInput);
+  const hr = document.createElement("hr");
 
-  radio1.appendChild(radioInput1);
-  radio2.appendChild(radioInput2);
-  radio3.appendChild(radioInput3);
-  nome.appendChild(radio1);
-  nome.appendChild(radio2);
-  nome.appendChild(radio3);
+  form.appendChild(h3);
+  form.appendChild(tecnologia);
+  form.appendChild(tech);
+  form.appendChild(time);
+  form.appendChild(input1);
+  form.appendChild(radio1);
+  form.appendChild(input2);
+  form.appendChild(radio2);
+  form.appendChild(input3);
+  form.appendChild(radio3);
+  form.appendChild(buttonExcluir);
+  section.append(form);
 
-  nome.appendChild(buttonExcluir);
-
-  nome.appendChild(hr);
-
-  form.append(nome);
-}
+  console.log(form);
+});
 
 const buttonC = document.getElementById("cadastrarDev");
 
-buttonC.addEventListener("click", cadastrar);
+buttonC.addEventListener("click", function (ev) {
+  ev.preventDefault();
 
-function cadastrar() {
-  const form = document.getElementById("input");
+  const Dev = {};
 
-  const nome = document.querySelector("input[name='Tecnologia']").value;
-  const check = document.querySelector("input[name='checado']").value;
+  const form = document.getElementById("section");
 
-  Devs.nome = nome;
-  Devs.check = check;
+  Dev.nome = document.querySelector("input[name='Tecnologia']").value;
+  Dev.check = document.querySelector("input[type='radio']:checked").value;
+
+  Devs.push(Dev);
 
   console.log(Devs);
-}
+});
